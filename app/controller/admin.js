c208a142e7388c34b1518f7601f84e9614f26af2
admin <xianglqqo@qq.com>
2017-05-09 20:16:41 +0800

    1

--- app/controller/admin.js
+++ app/controller/admin.js
@@ -1,8 +1,15 @@
 module.exports = ctx => {
-  let response = ctx.getModel('tests').insert({
-    'emp_id': '2',
-    'nick': '小明',
-    'department': '技术部'
-  })
-  ctx.body = 'admin'
+  return {
+    async index() {
+      let response = await ctx.getModel('tests').insert({
+        'emp_id': '2',
+        'nick': '小明',
+        'department': '技术部'
+      })
+      ctx.body = response;
+    },
+    test() {
+      ctx.body = 'Hello goods test';
+    }
+  }
 }
