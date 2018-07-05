f78cb1cf14fd4cd48e0f3aac9c33d9c47af1e787
LiXiang <xianglqqo@gmail.com>
2018-07-05 13:28:58 +0800

    更新batch-import库，修改loader加载器

--- core/loader.js
+++ core/loader.js
@@ -1,64 +1,63 @@
 let batchImport = require('batch-import')
-let app = require('./index')
-let config = require('./config')
+let app = require('./index.js')
+let config = require('./config.js')
 
-
-// 自定义模块批量加载器
-app.loader = function (options) {
-   Object.assign(app, batchImport(options))
-}
-
-// 批量加载模块
+// 批量预加载模块
 batchImport({
    "config": {
       "path": "config/"
    },
-   // "plugin": {
-   //    "path": "plugin/",
-   //    process(name, func) {
-   //       if (func instanceof Function) {
-   //          return func(this)
-   //       } else {
-   //          throw new Error(`${name}插件输出数据类型必须为函数`)
-   //       }
-   //    }
-   // },
+   "plugin": {
+      "path": "plugin/",
+      import(filename, func) {
+         // if (func instanceof Function) {
+         //    return func(this)
+         // } else {
+         //    throw new Error(`${filename}插件输出数据类型必须为函数`)
+         // }
+      }
+   },
    "extend": {
       "path": "app/extend",
-      process(name, data) {
+      import(filename, data) {
          // 如果导出为函数类型，则判定为依赖注入函数
          if (data instanceof Function) {
             data = data(this)
          }
-         // 对扩展进行扁平化处理，缩短访问路径
-         this[name] = data
+         return data
+      },
+      complete(data) {
+         // 对extend进行扁平化处理，缩短访问路径
+         for (let name in data) {
+            this[name] = data[name]
+         }
          return data
       }
    },
    "models": {
       "path": "app/models/",
-      process(name, func) {
+      import(filename, func) {
          if (func instanceof Function) {
             return func(this)
          } else {
-            throw new Error(`${name}模块导出必须为函数`)
+            throw new Error(`${filename}模块导出必须为函数`)
          }
       }
    },
    "middleware": {
       "path": "app/middleware/",
-      process(name, func) {
+      import(filename, func) {
          if (func instanceof Function) {
             // middleware保持原样输出，不执行函数
             return func
          } else {
-            throw new Error(`${name}模块导出必须为注入函数`)
+            throw new Error(`${filename}模块导出必须为注入函数`)
          }
       }
    },
    "controller": {
       "path": "app/controller/",
-      process(name, func) {
+      import(filename, func) {
          if (func instanceof Function) {
             // 普通函数，不管是不是构造函数都可以使用new
             if (func.prototype) {
@@ -69,18 +68,23 @@ batchImport({
                return func(this)
             }
          } else {
-            throw new Error(`${name}模块导出必须为注入函数`)
+            throw new Error(`${filename}模块导出必须为注入函数`)
          }
       }
-   },
+   }
 }, app)
 
-// 合并配置项（未添加环境变量，暂时使用固定配置）
+// 合并配置项（未添加环境变量，临时使用固定配置）
 Object.assign(config, app.config.default)
 
 Object.assign(config, app.config['localhost'])
 
 app.config = config
+
+// 自定义模块批量加载器
+app.loader = function (options) {
+   Object.assign(app, batchImport(options))
+}
 
 // 全局配置中间件转换
 let middlewares = app.config.middlewares
