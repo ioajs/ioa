0c825757d2f45f17382c3133462e1add3764cccf
LiXiang <xianglqqo@gmail.com>
2019-04-21 11:48:02 +0800

    修复NODE_ENV变量匹配失败，去除首尾空格

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const logger = require('loggercc');
 const loader = require('./lib/loader.js');
 const { version } = require('./package.json');
 
-const NODE_ENV = process.env.NODE_ENV || 'production';
+let { NODE_ENV } = process.env;
+
+if (NODE_ENV) {
+   NODE_ENV = NODE_ENV.trim();
+} else {
+   NODE_ENV = 'production';
+}
 
 const ioa = {
    version,
@@ -17,6 +23,7 @@ const ioa = {
 }
 
 console.log('');
-logger.log(`NODE_ENV = ${NODE_ENV}`);
+
+logger.log(`NODE_ENV = '${NODE_ENV}'`);
 
 module.exports = ioa;
