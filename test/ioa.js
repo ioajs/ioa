d8daeb4bb8564bf50349d5435f5429a3803da294
lixiang <lixiangio@qq.com>
2021-08-30 20:02:10 +0800

    v12.0.6

--- test/ioa.js
+++ test/ioa.js
@@ -2,119 +2,119 @@ import test from 'jtm';
 import typea from 'typea';
 import ioa from 'ioa';
 
-test('ioa', t => {
-
-  const schema = typea({
-    argv: Object,
-    version: String,
-    NODE_ENV: String,
-    apps: Function,
-    components: {
-      '@ioa/config': Object,
-      '@ioa/koa': Object,
-      '@common': Object,
-    },
-    applications: {
-      main: {
-        middlewareBefore: [Function],
-        router: {
-          get: Function,
-          post: Function,
-          put: Function,
-          delete: Function,
-          resources: Function
-        },
-        config: {
-          "mixin": {
-            a: 666
-          }
-        },
-        model: {
-          document: Object
-        },
-        middleware: {
-          cors: Function,
-          token: Function
-        },
-        controller: {
-          home: {
-            index: Function,
-            sms: Function,
-            login: Function,
-          },
-          index: {
-            test: Function,
-          },
-          news: {
-            index: Function,
-            details: Function,
-          },
-          rest: {
-            index: Function,
-            details: Function,
-            create: Function,
-            update: Function,
-            destroy: Function
-          },
-          test: {
-            index: Function,
-            details: Function,
-            create: Function,
-            update: Function,
-            destroy: Function
-          }
-        },
-        test: 666
+const schema = typea({
+  argv: Object,
+  version: String,
+  NODE_ENV: String,
+  apps: Function,
+  components: {
+    '@ioa/config': Object,
+    '@ioa/koa': Object,
+    '@common': Object,
+  },
+  applications: {
+    main: {
+      middlewareBefore: [Function],
+      router: {
+        get: Function,
+        post: Function,
+        put: Function,
+        delete: Function,
+        resources: Function
       },
-      admin: {
-        middlewareBefore: [Function],
-        router: {
-          get: Function,
-          post: Function,
-          put: Function,
-          delete: Function,
-          resources: Function
-        },
-        config: {
-          base: String
-        },
-        model: { compcerts: undefined },
-        middleware: {
-          token: Function,
-        },
-        controller: {
-          home: {
-            index: Function,
-            details: Function,
-            add: Function,
-            update: Function,
-            delete: Function
-          }
-        },
+      config: {
+        "mixin": {
+          a: 666
+        }
       },
-      user: {
-        middlewareBefore: [Function],
-        router: {
-          get: Function,
-          post: Function,
-          put: Function,
-          delete: Function,
-          resources: Function
+      model: {
+        document: Object
+      },
+      middleware: {
+        cors: Function,
+        token: Function
+      },
+      controller: {
+        home: {
+          index: Function,
+          sms: Function,
+          login: Function,
         },
-        config: {},
-        middleware: {
+        index: {
           test: Function,
-          intercept: Function
         },
-        controller: {
-          home: {
-            home: Function,
-          }
+        news: {
+          index: Function,
+          details: Function,
+        },
+        rest: {
+          index: Function,
+          details: Function,
+          create: Function,
+          update: Function,
+          destroy: Function
+        },
+        test: {
+          index: Function,
+          details: Function,
+          create: Function,
+          update: Function,
+          destroy: Function
+        }
+      },
+      test: 666
+    },
+    admin: {
+      middlewareBefore: [Function],
+      router: {
+        get: Function,
+        post: Function,
+        put: Function,
+        delete: Function,
+        resources: Function
+      },
+      config: {
+        base: String
+      },
+      model: { compcerts: undefined },
+      middleware: {
+        token: Function,
+      },
+      controller: {
+        home: {
+          index: Function,
+          details: Function,
+          add: Function,
+          update: Function,
+          delete: Function
         }
       },
     },
-    main: Object,
-  });
+    user: {
+      middlewareBefore: [Function],
+      router: {
+        get: Function,
+        post: Function,
+        put: Function,
+        delete: Function,
+        resources: Function
+      },
+      config: {},
+      middleware: {
+        test: Function,
+        intercept: Function
+      },
+      controller: {
+        home: {
+          home: Function,
+        }
+      }
+    },
+  },
+  main: Object,
+});
+
+test('ioa', t => {
 
   const { data, error } = schema.strictVerify(ioa);
 
